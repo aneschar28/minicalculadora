9e4344e26ca56a743641b6f1cb4e962d20642c11
aneschar28 <aechavarriaa1128@gmail.com>
2025-12-16 21:51:18 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 const slider = document.getElementById("slider");
+
 const labeltop75 = document.getElementById("label-top-75");
 const labeltop2_75 = document.getElementById("label-top2-75");
 const labeltop80 = document.getElementById("label-top-80");
@@ -9,10 +10,12 @@ const labeltop = document.getElementById("label-top");
 const labeltop2 = document.getElementById("label-top2");
 const labelBottom = document.getElementById("label-bottom");
 const labelBottom2 = document.getElementById("label-bottom2");
+
 const valor1 = document.getElementById("valor1");
 const valor2 = document.getElementById("valor2");
 const valor3 = document.getElementById("valor3");
 const valor4 = document.getElementById("valor4");
+
 const button = document.getElementById("button");
 const deletebotton = document.getElementById("delete");
 
@@ -65,6 +68,7 @@ const ad5000Prefilter = document.getElementById("ad5000Prefilter");
 
 function calcular2() {
     slider.value = valor4.value;
+
     labeltop90.innerHTML = `${(Number(valor2.value) * 0.90).toFixed(2)}%`;
     labeltop2_90.innerHTML = `${(Number(valor1.value) * 0.90).toFixed(2)}$`;
 
@@ -76,8 +80,9 @@ function calcular2() {
 
     labeltop.innerHTML = `${(Number(valor2.value) * 1).toFixed(2)}%`;
     labeltop2.innerHTML = `${(Number(valor1.value) * 1).toFixed(2)}$`;
-    labelBottom.innerHTML = `${0}%`;
-    labelBottom2.innerHTML = `${0}$`;
+
+    labelBottom.innerHTML = `0%`;
+    labelBottom2.innerHTML = `0$`;
 
     slider.value = 0;
     labelBottom.style.left = "0px";
@@ -85,16 +90,15 @@ function calcular2() {
 }
 
 function calcular() {
-
-    if (Number(valor2.value)>100) {
-
+    if (Number(valor2.value) > 100) {
         alert("el total no puede ser mas del 100%");
-        
     }
 
-    const ok = [valor1.value, valor2.value, valor3.value, valor4.value].filter(Boolean).length;
+    const ok = [valor1.value, valor2.value, valor3.value, valor4.value]
+        .filter(Boolean).length;
 
     if (ok === 3) {
+
         if (valor4.value === "") {
             valor4.value = ((Number(valor2.value) * Number(valor3.value)) / Number(valor1.value)).toFixed(2);
         }
@@ -122,10 +126,12 @@ function calcular() {
 
         labeltop.innerHTML = `${(Number(valor2.value) * 1).toFixed(2)}%`;
         labeltop2.innerHTML = `${(Number(valor1.value) * 1).toFixed(2)}$`;
+
         labelBottom.innerHTML = `${valor4.value}%`;
         labelBottom2.innerHTML = `${valor3.value}$`;
 
         slider.value = valor4.value;
+
         const value = slider.value;
         const sliderWidth = slider.offsetWidth;
         const thumbPos = (value / slider.max) * sliderWidth;
@@ -150,8 +156,8 @@ function updateLabels() {
     labelBottom.innerHTML = `${value}%`;
     labelBottom2.innerHTML = `${Number(((value / 100) * Number(valor1.value)).toFixed(2))}$`;
 
-    labelBottom.style.left = thumbPos +20+ "px";
-    labelBottom2.style.left = thumbPos +20+ "px";
+    labelBottom.style.left = thumbPos + 20 + "px";
+    labelBottom2.style.left = thumbPos + 20 + "px";
 }
 
 function buttonSkip(numeros) {
@@ -159,6 +165,7 @@ function buttonSkip(numeros) {
     valor2.value = "100";
     valor3.value = "";
     valor4.value = "";
+
     labeltop90.innerHTML = `${(Number(valor2.value) * 0.90).toFixed(2)}%`;
     labeltop2_90.innerHTML = `${(Number(valor1.value) * 0.90).toFixed(2)}$`;
 
@@ -170,11 +177,48 @@ function buttonSkip(numeros) {
 
     labeltop.innerHTML = `${(Number(valor2.value) * 1).toFixed(2)}%`;
     labeltop2.innerHTML = `${(Number(valor1.value) * 1).toFixed(2)}$`;
+
     slider.value = "0";
     labelBottom.style.left = "0px";
     labelBottom2.style.left = "0px";
     labelBottom.innerHTML = "0%";
     labelBottom2.innerHTML = "0$";
+}
+
+// RP = regular price
+// CSP = customer service price
+// MP = minimum price
+// AP = afiliate price
+
+function unitsInfo(RP, CSP, MP, AP) {
+
+    function setLabel(container, text, title = null) {
+        const p = document.createElement("p");
+        p.textContent = text;
+
+        if (title) {
+            p.title = title;
+        }
+
+        container.innerHTML = "";
+        container.appendChild(p);
+    }
+
+    // Regular Price
+    setLabel(labeltop, "RP", "Regular Price");
+    setLabel(labeltop2, RP);
+
+    // CSP
+    setLabel(labeltop90, "CSP");
+    setLabel(labeltop2_90, CSP);
+
+    // MP
+    setLabel(labeltop80, "MP");
+    setLabel(labeltop2_80, MP);
+
+    // AP
+    setLabel(labeltop75, "AP");
+    setLabel(labeltop2_75, AP);
 }
 
 updateLabels();
@@ -218,25 +262,25 @@ walkfitPlatinum.addEventListener("click", () => {
 
 // AirDoctor Unidades
 ad1000.addEventListener("click", () => {
-    buttonSkip(419);
+    unitsInfo(700, 400, 359, 330);
 });
 ad2000.addEventListener("click", () => {
-    buttonSkip(409);
+    unitsInfo(700, 400, 359, 330);
 });
 ad3500.addEventListener("click", () => {
-    buttonSkip(659);
+    unitsInfo(500, 400, 359, 330);
 });
 ad3500i.addEventListener("click", () => {
-    buttonSkip(709);
+    unitsInfo(700, 400, 359, 330);
 });
 ad4000.addEventListener("click", () => {
-    buttonSkip(799);
+    unitsInfo(800, 400, 359, 330);
 });
 ad5500.addEventListener("click", () => {
-    buttonSkip(999);
+    unitsInfo(720, 400, 359, 330);
 });
 ad5500i.addEventListener("click", () => {
-    buttonSkip(1049);
+    unitsInfo(700, 400, 359, 330);
 });
 
 // Filtros AD 1000
@@ -299,4 +343,3 @@ ad5000Carbon.addEventListener("click", () => {
 ad5000Prefilter.addEventListener("click", () => {
     buttonSkip(35);
 });
-
